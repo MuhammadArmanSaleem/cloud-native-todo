0ce7f2ec6ffde989ac4df6820585c0297afb3a07
MuhammadArmanSaleem <armansaleem326@gmail.com>
2026-01-08 16:41:08 +0500

    deleted/ingored the next

--- phase-2/frontend/.next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__c01f9a4f._.css",
-  "static/chunks/_44402967._.js"
-],
-    source: "dynamic"
-});
