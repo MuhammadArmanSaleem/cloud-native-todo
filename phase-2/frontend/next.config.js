f03ae55a89583ef192ffcdfca4bf90a6c03e6ec7
MuhammadArmanSaleem <armansaleem326@gmail.com>
2026-02-08 19:19:21 +0500

    feat:done phase 4

--- phase-2/frontend/next.config.js
+++ phase-2/frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const backendUrl = process.env.BACKEND_URL || 'http://127.0.0.1:8001';
 const nextConfig = {
+  output: 'standalone',
   typedRoutes: false,
   images: {
     remotePatterns: [
